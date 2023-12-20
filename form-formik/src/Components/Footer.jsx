eda1127f5fa8a40cf77aeeb049c19e4dae08a0c8
Subhikathir <subhikshaphy1821@gmail.com>
2023-12-20 18:26:15 +0530

    first commit

---
+++ form-formik/src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white text-center py-3">
+      <div className="container">
+        <p className="mb-0">
+        
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ form-formik/src/Pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <>
+    <div className="d-flex flex-column ml-1 justify-content-center align-items-center min-vh-100">
+      <h1 className="display-5 fw-bold lh-1  mb-5">LIBRARY MANAGEMENT SYSTEM</h1>
+      
+    
+    </div>
+    </>
+  );
+};
+
+export default Homepage;
